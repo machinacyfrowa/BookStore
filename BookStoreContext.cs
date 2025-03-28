7fc46f9ba86337d2834507cd94c12c6e5c1107fc
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2025-03-28 11:21:43 +0100

    Dodaj pliki projektów.

---
+++ BookStoreContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    public class BookStoreContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=bookstore.db");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //definiujemy zależności pomiedzy tabelami
+            modelBuilder.Entity<Author>()
+                //jeden autor ma wiele książek
+                .HasMany(a => a.Books)
+                //jedna książka ma jednego autora
+                .WithOne(b => b.Author)
+                //klucz obcy
+                .HasForeignKey(b => b.AuthorId);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using BookStore;
+using System.Net.Http.Headers;
+
+//static void Main()
+//{
+
+//}
+
+using (var context = new BookStoreContext())
+{
+    context.Database.EnsureDeleted();
+    context.Database.EnsureCreated();
+    var author = new Author
+    {
+        Name = "George R.R. Martin",
+        Books = new List<Book>()
+
+    };
+    Book book = new Book { Title = "Game of Thrones", Author = author };
+
+    context.Authors.Add(author);
+    context.Books.Add(book);
+    context.SaveChanges();
+
+    var authors = context.Authors.ToList();
+    foreach(var a in authors)
+    {
+        Console.WriteLine("Autor: " + a.Name);
+        Console.WriteLine("Książki: ");
+        foreach (var b in a.Books)
+        {
+            Console.WriteLine("   " + b.Title);
+        }
+    }
+}
